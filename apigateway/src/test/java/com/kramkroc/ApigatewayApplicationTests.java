603e79f95efd42e237b1bfb0bd70b045069b15d8
Mark Corkery <mark.corkery@gmail.com>
2016-03-15 10:18:33 +0000

    Adding docker-compose info to run this up in docker.

--- apigateway/src/test/java/com/kramkroc/ApigatewayApplicationTests.java
+++ apigateway/src/test/java/com/kramkroc/ApigatewayApplicationTests.java
@@ -4,9 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.WebIntegrationTest;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ApigatewayApplication.class)
+@WebIntegrationTest({ "server.port:0", "eureka.client.enabled:false",
+        "spring.cloud.config.enabled:false", "spring.cloud.config.discovery.enabled:false",
+        "security.basic.enabled:false" })
 public class ApigatewayApplicationTests {
 
 	@Test
